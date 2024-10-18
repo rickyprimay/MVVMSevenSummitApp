45a1fafbc5fa430f45c458b345a323b2cfc44a45
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-10-18 19:08:35 +0700

    Initial Commit

---
+++ SevenSummitMountainCentralJava/SevenSummitMountainCentralJavaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SevenSummitMountainCentralJavaApp.swift
+//  SevenSummitMountainCentralJava
+//
+//  Created by Ricky Primayuda Putra on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SevenSummitMountainCentralJavaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
